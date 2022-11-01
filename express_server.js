0c87127a342867661cceebfafd50aa1fcc14235b
Andy Farquharson <andyfarquharson@gmail.com>
2022-11-01 19:49:40 +0000

    Using nodemon to keep our server open when making edits

--- express_server.js
+++ express_server.js
@@ -10,36 +10,37 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
-
+// Homepage
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+// Hellopage
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
 
-app.get("/urls", (req, res) => {
+app.post("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
+  console.log(req.body); // Log the POST request body to the console
   res.render("urls_index", templateVars);
-})
+  res.redirect("/urls/:id"); // Redirects the user to the id page
+});
+// Database
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
+app.get("/u/:id", (req, res) => {
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: req.params.id };
   res.render("urls_show", templateVars);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const longURL = req.params.id
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
