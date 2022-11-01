48c36e5c9db2ee8658419b0fa520b02fdb494b13
Andy Farquharson <andyfarquharson@gmail.com>
2022-11-01 21:08:02 +0000

    Added in the long url

--- express_server.js
+++ express_server.js
@@ -5,6 +5,10 @@ const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
+function generateRandomString() {
+  const result = Math.random().toString(36).substring(2,8);
+return result;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,43 +16,39 @@ const urlDatabase = {
 };
 // Homepage
 app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-// Hellopage
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  res.redirect("/urls");
 });
 
 app.post("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  console.log(req.body); // Log the POST request body to the console
-  res.render("urls_index", templateVars);
-  res.redirect("/urls/:id"); // Redirects the user to the id page
-});
-// Database
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+  const shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL
+  res.redirect(`/urls/${shortURL}`);
 });
 
+// Form to get a new url
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.get("/u/:id", (req, res) => {
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+})
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls/:id", (req, res) => {
   const templateVars = { 
     id: req.params.id, 
-    longURL: req.params.id };
+    longURL: req.body.longURL };
   res.render("urls_show", templateVars);
-  const longURL = req.params.id
-  res.redirect(longURL);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-function generateRandomString() {
-  const result = Math.random().toString(36).substring(1,7);
-return result;
-}
-generateRandomString();
+
