92a203357b763db62d2a44328228489301b5e159
Andy Farquharson <andyfarquharson@gmail.com>
2022-11-01 02:05:58 +0000

    Added an add new urls page

--- express_server.js
+++ express_server.js
@@ -18,13 +18,17 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 })
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
-  res.render("urls_show", templateVars);
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
